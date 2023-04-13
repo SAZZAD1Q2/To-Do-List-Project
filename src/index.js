88681ec2c0897ffa59a94b85deffaaac84ac4d2a
SAZZAD1Q2 <kurulia115@gmail.com>
2023-04-13 22:17:42 +0600

    html and css file modified

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-import "./style.css";
-console.log("Hello webpack!")
+import './style.css';
+
+const taskInput = document.getElementById('input');
+const addButton = document.getElementById('button');
+const todoList = document.getElementById('todo-list');
+
+//  add work
+
+const removeTask = (event) => {
+  const li = event.target.parentNode;
+  todoList.removeChild(li);
+};
+
+const addTask = () => {
+  const taskValue = taskInput.value.trim();
+
+  const li = document.createElement('li');
+  li.innerHTML = `${taskValue} <button class="remove-button">X</button>`;
+  todoList.appendChild(li);
+
+  taskInput.value = '';
+  const removeButton = li.querySelector('.remove-button');
+  removeButton.addEventListener('click', removeTask);
+};
+
+addButton.addEventListener('click', addTask);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: '/src/index.js',
+  mode: 'development',
+  entry: '/src/index.js',
 
   output: {
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    })
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
   ],
 };
