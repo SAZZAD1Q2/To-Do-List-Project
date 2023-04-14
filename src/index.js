8f91453eacfd0932f42708c4ef238471896844f7
SAZZAD1Q2 <kurulia115@gmail.com>
2023-04-14 15:24:35 +0600

    js file edited

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 const tasks = [
   {
-    description: 'Task 1',
+    description: 'Wash the dishes',
     completed: false,
     index: 1,
   },
   {
-    description: 'Task 2',
+    description: 'Completed TO Do list project',
     completed: true,
     index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
   },
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
   const todoList = document.getElementById('todo-list');
   todoList.innerHTML = '';
-  todoList.style.listStyle= 'none';
+  todoList.style.listStyle = 'none';
 
   tasks.sort((a, b) => a.index - b.index);
 
   tasks.forEach((task) => {
     const li = document.createElement('li');
+    li.style.border = '1px solid #ddd';
+    li.style.display = 'block'
+    li.style.padding = '10px 0px 10px 50px';
+    li.style.marginLeft = '-40px';
+    li.style.marginRight = '50vw';
+
+
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = task.completed;
