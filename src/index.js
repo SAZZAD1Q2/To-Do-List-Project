f44003615de9d9a67a938eb815b6d357c3819239
SAZZAD1Q2 <kurulia115@gmail.com>
2023-04-15 00:28:41 +0600

    js file edited

--- src/index.js
+++ src/index.js
@@ -1,36 +1,76 @@
-const tasks = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Completed TO Do list project',
-    completed: true,
-    index: 2,
-  },
-];
+import './style.css';
 
+const taskInput = document.getElementById('input');
+const addButton = document.getElementById('button');
+const todoList = document.getElementById('todo-list');
+const completeButton = document.querySelector('.complete');
+
+// add task
 document.addEventListener('DOMContentLoaded', () => {
-  const todoList = document.getElementById('todo-list');
-  todoList.innerHTML = '';
-  todoList.style.listStyle = 'none';
+  const removeTask = (event) => {
+    const li = event.target.parentNode;
+    todoList.removeChild(li);
+  };
 
-  tasks.sort((a, b) => a.index - b.index);
+  const editTask = (event) => {
+    const li = event.target.parentNode;
+    const taskValue = li.firstChild.textContent.trim();
+    const newTaskValue = prompt('Edit task:', taskValue);
+    if (newTaskValue !== null && newTaskValue.trim() !== '') {
+      li.firstChild.textContent = newTaskValue.trim();
+    } else {
+      return;
+    }
 
-  tasks.forEach((task) => {
+    const popupDiv = document.createElement('div');
+    popupDiv.classList.add('popup');
+    const heading = document.createElement('h2');
+    heading.textContent = 'Edit task';
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = newTaskValue.trim();
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Save';
+    saveButton.addEventListener('click', () => {
+      const editedTaskValue = input.value.trim();
+      if (editedTaskValue !== '') {
+        li.firstChild.textContent = editedTaskValue;
+        popupDiv.remove();
+      }
+    });
+    popupDiv.appendChild(heading);
+    popupDiv.appendChild(input);
+    popupDiv.appendChild(saveButton);
+    document.body.appendChild(popupDiv);
+  };
+
+  const addTask = () => {
+    const taskValue = taskInput.value.trim();
+
     const li = document.createElement('li');
-    li.style.border = '1px solid #ddd';
-    li.style.display = 'block';
-    li.style.padding = '10px 0px 10px 50px';
-    li.style.marginLeft = '-40px';
-    li.style.marginRight = '50vw';
-
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.checked = task.completed;
     li.appendChild(checkbox);
-    li.innerHTML += task.description;
+    li.innerHTML += `${taskValue} <button class="remove-button">X</button> <button class="edit-button">Edit</button>`;
     todoList.appendChild(li);
-  });
+
+    taskInput.value = '';
+    const removeButton = li.querySelector('.remove-button');
+    const editButton = li.querySelector('.edit-button');
+    removeButton.addEventListener('click', removeTask);
+    editButton.addEventListener('click', editTask);
+  };
+
+  const removeCompletedTasks = () => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        const li = checkbox.parentNode;
+        todoList.removeChild(li);
+      }
+    });
+  };
+
+  addButton.addEventListener('click', addTask);
+  completeButton.addEventListener('click', removeCompletedTasks);
 });
